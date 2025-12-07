f81a893499d4a63d200db8517ac6ddb55886bfc7
jhonstolarski-lang <226617180+jhonstolarski-lang@users.noreply.github.com>
2025-12-07 07:21:47 -0500

    fix: Corrige alias @assets no vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
       "@shared": path.resolve(__dirname, "shared"),
-      "@assets": path.resolve(import.meta.dirname, "attached_assets"),
+      "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
   envDir: path.resolve(import.meta.dirname),
